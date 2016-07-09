ddf4c18a2b07e97f237aa4096d4aff3f79859654
Breno Nunes <breno.arantes97@gmail.com>
2016-07-09 11:41:17 -0300

    Aula 2 - Métodos e Actions
    Conceito dos methods POST e GET do HTML.
    Como implementar este methods no ASP.
    Uso dos ActionsResult
    Troca de dados entre Views.

---
+++ CleytonFerrari/PostGetModel/PostGetModel/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using PostGetModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostGetModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //não precisa colocar o [HttpGet] pois todas as actions são get
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa //este objeto foi criado em models, a classe pessoa
+            {
+                PessoaId = 1,
+                Nome = "Breno",
+                Twitter = "@brenovit"
+            };
+
+            var pessoa2 = new Pessoa //este objeto foi criado em models, a classe pessoa
+            {
+                PessoaId = 2,
+                Nome = "Cleyton",
+                Twitter = "@cleyFerrari"
+            };
+
+            var pessoa3 = new Pessoa //este objeto foi criado em models, a classe pessoa
+            {
+                PessoaId = 3,
+                Nome = "Rafael",
+                Twitter = "@rafa"
+            };
+
+            //atravez do ViewData estou enviando os dados do objeto acima para minha view
+            ViewData["PessoaId"] = pessoa.PessoaId; //variavei dinamica, array          
+            ViewData["Twitter"] = pessoa.Twitter;
+            ViewData["Nome"] = pessoa.Nome;
+
+            ViewBag.pid = pessoa2.PessoaId; //variavei dinamica, array          
+            ViewBag.ptwit = pessoa2.Twitter;
+            ViewBag.pnome = pessoa2.Nome;
+
+            return View(pessoa3);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            /*ViewData["PessoaId"] = form["pId"];
+            ViewData["Nome"] = form["pNome"];
+            ViewData["Twitter"] = form["pTwitter"];*/
+
+            /*ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Twitter"] = pessoa.Twitter;
+            ViewData["Nome"] = pessoa.Nome;*/
+            
+            return View(pessoa);
+        }
+    }
+}
---
+++ CleytonFerrari/PostGetModel/PostGetModel/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace PostGetModel.Models
+{
+    public class Pessoa
+    {
+        //removido todas as bibliotecas
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Twitter { get; set; }
+    }
+}
