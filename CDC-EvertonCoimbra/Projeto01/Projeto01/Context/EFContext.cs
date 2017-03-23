9afcaa7520e34c831e66fe8d55ebe49eca5cef0e
Breno Nunes <breno.arantes97@gmail.com>
2017-03-23 14:02:28 -0300

    ASSOCIAÇÕES NO ENTITY FRAMEWORK

--- CDC-EvertonCoimbra/Projeto01/Projeto01/Context/EFContext.cs
+++ CDC-EvertonCoimbra/Projeto01/Projeto01/Context/EFContext.cs
@@ -9,9 +9,12 @@ namespace Projeto01.Context
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CD") { }
+        public EFContext() : base("Asp_Net_MVC_CD") {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
---
+++ CDC-EvertonCoimbra/Projeto01/Projeto01/Controllers/ProdutosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Context;
+using Projeto01.Models;
+
+namespace Projeto01.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private EFContext db = new EFContext();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            var produtos = db.Produtos.Include(p => p.Categoria).Include(p => p.Fabricante);
+            return View(produtos.ToList().OrderBy(n => n.Nome));
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Where(p => p.ProdutoID == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoriaID = new SelectList(db.Categorias, "CategoriaID", "Nome");
+            ViewBag.FabricanteID = new SelectList(db.Fabricantes, "FabricanteID", "Nome");
+            return View();
+        }
+
+        // POST: Produtos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProdutoID,Nome,CategoriaID,FabricanteID")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produtos.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoriaID = new SelectList(db.Categorias, "CategoriaID", "Nome", produto.CategoriaID);
+            ViewBag.FabricanteID = new SelectList(db.Fabricantes, "FabricanteID", "Nome", produto.FabricanteID);
+            return View(produto);
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaID = new SelectList(db.Categorias, "CategoriaID", "Nome", produto.CategoriaID);
+            ViewBag.FabricanteID = new SelectList(db.Fabricantes, "FabricanteID", "Nome", produto.FabricanteID);
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProdutoID,Nome,CategoriaID,FabricanteID")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoriaID = new SelectList(db.Categorias, "CategoriaID", "Nome", produto.CategoriaID);
+            ViewBag.FabricanteID = new SelectList(db.Fabricantes, "FabricanteID", "Nome", produto.FabricanteID);
+            return View(produto);
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produtos.Where(p => p.ProdutoID == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Produto produto = db.Produtos.Find(id);
+            db.Produtos.Remove(produto);
+            db.SaveChanges();
+            TempData["Message"] = $"Produto {produto.Nome.ToUpper()} foi removido";
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
